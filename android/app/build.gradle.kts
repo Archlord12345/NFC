0710e5c8cf28d7798f388ff2b9c9ea280801816a
archlordroy-codeur <archlordroy@gmail.com>
2026-06-10 02:44:54 +0100

    fix(build): upgrade plugins and migrate to Java 21

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,13 +9,13 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     kotlin {
         compilerOptions {
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
         }
     }
 
