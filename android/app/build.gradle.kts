9b21333638e4430de0ac503373489a451761a839
archlordroy-codeur <archlordroy@gmail.com>
2026-06-10 01:19:59 +0100

    fix(android): migrate kotlinOptions to compilerOptions DSL

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,8 +14,10 @@ android {
         targetCompatibility = JavaVersion.VERSION_21
     }
 
-    kotlinOptions {
-        jvmTarget = "21"
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
+        }
     }
 
     defaultConfig {
